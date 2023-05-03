d84e7c94c77daad5fb2c1e470888a39a540bbe64
Kostadin Stanev <stanebvns@gmail.com>
2023-05-03 13:31:43 +0300

    adding Redux Toolkit

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import cartReducer from '../components/Cart/CartSlice';
+import cartReducer from './CartSlice';
 export default configureStore({
   reducer: {
     cart: cartReducer,
--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,5 +1,20 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { add_item } from '../../app/CartSlice';
+
 const Cart = () => {
-  return <p>Cart</p>;
+  const cartTotal = useSelector((state) => state.cart.total);
+  const dispatch = useDispatch();
+
+  const increaseTotal = () => {
+    dispatch(add_item());
+  };
+
+  return (
+    <h1>
+      Cart {cartTotal}
+      <button onClick={increaseTotal}>CLICK ME</button>
+    </h1>
+  );
 };
 
 export default Cart;
