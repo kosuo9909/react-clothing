6825ff56f5245e28fb2779350aa91b9e622ef396
Kostadin Stanev <stanebvns@gmail.com>
2023-05-11 10:54:03 +0300

    made hero responsive

--- src/components/UI/PrimaryHeading.js
+++ src/components/UI/PrimaryHeading.js
@@ -1,15 +1,22 @@
 import { Fragment } from 'react';
 import styles from './PrimaryHeading.module.css';
-import { Link, NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const PrimaryHeading = () => {
   return (
     <Fragment>
       <section className={styles.vh100}>
         <div className={styles.hero}>
+          <div className={styles.heroImg}>
+            <img
+              className={styles.headingImg}
+              src='https://i.imgur.com/oUOiyco.jpg'
+              alt='A Fashionable woman'
+            ></img>
+          </div>
           <div className={styles.primaryBox}>
             <h1 className={styles.headingPrimary}>
-              Мобилното ти приключение започва тук.
+              Мобилното приключение започва тук.
             </h1>
 
             <p className={styles.headingDescription}>
@@ -19,13 +26,6 @@ const PrimaryHeading = () => {
             <Link to='/shop' className={styles.headingBtn}>
               Насладете се на иновациите{' '}
             </Link>
-          </div>
-          <div className={styles.heroImg}>
-            <img
-              className={styles.headingImg}
-              src='https://i.imgur.com/oUOiyco.jpg'
-              alt='A Fashionable woman'
-            ></img>
           </div>
         </div>
       </section>
